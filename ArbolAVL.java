8d49adafd840ed630b0115b89c222fa08b848e05
Luis Manuel Verdugo <43182044+luismanuelverdugo@users.noreply.github.com>
2025-11-24 19:50:40 -0700

    Add files via upload

--- ArbolAVL.java
+++ ArbolAVL.java
@@ -82,4 +82,89 @@ public class ArbolAVL {
         //nueva raiz del subarbol
         return y;
     }
+
+    /*
+    inserta un valor en el arbol avl respetando las reglas del
+    arbol binario, luego verifica si el arbol quedo desbalanceado,
+    de ser asi, ejecuta la rotacion correspondiente
+     */
+    NodoAVL insertar(NodoAVL nodo, int valor){
+        //1. insercion normal de AB = Arbol binario
+        if(nodo==null)
+            return new NodoAVL(valor);
+        if(valor<nodo.valor)
+            nodo.izquierda=insertar(nodo.izquierda, valor);
+        else if(valor>nodo.valor)
+            nodo.derecha=insertar(nodo.derecha, valor);
+        else
+            return nodo; //no se permiten valores duplicados
+
+        //2. actializar la altura del nodo padre
+        nodo.altura=1+max(altura(nodo.izquierda), altura(nodo.derecha));
+    
+        //3. calcular el factor de balance
+        int balance = obtenerBalance(nodo);
+
+        //caso izquierda-izquierda
+        if(balance > 1 && valor < nodo.izquierda.valor)
+            return rotacionDerecha(nodo);
+        //caso derecha-derecha
+        if(balance < 1 && valor > nodo.derecha.valor)
+            return rotacionIzquierda(nodo);
+
+        //caso izquierda-derecha
+        if(balance > 1 && valor > nodo.izquierda.valor){
+            nodo.izquierda=rotacionIzquierda(nodo.izquierda);
+            return rotacionDerecha(nodo);
+        }
+
+        //caso derecha-izquierda
+        if(balance < -1 && valor < nodo.derecha.valor){
+            nodo.derecha=rotacionDerecha(nodo.derecha);
+            return rotacionIzquierda(nodo);
+        }
+
+        //si es balanceado, no se le caen los pajaritos "se retorna sin cambios"
+        return nodo;
+    }
+
+    //metodo publico para insertar desde el main
+    public void insertar(int valor){
+        raiz = insertar(raiz, valor);
+    }
+
+    /* 
+    el recorrido inorden imprime los valores en orden ascendente
+    1. recorre subarbol izquierdo
+    2. imprime nodo actual
+    3. recorre subarbol derecho
+    */
+    void inorden(NodoAVL nodo){
+        //entre solo si el arbol tiene nodos
+        if(nodo!=null){
+            inorden(nodo.izquierda);
+            System.out.print(nodo.valor+" ");
+            inorden(nodo.derecha);
+        }
+    }
+
+    public void mostrarInorden(){
+        inorden(raiz);
+        System.out.print(" ");
+    }
+
+    public static void main(String[] args){
+        //crear arbol
+        ArbolAVL arbol = new ArbolAVL();
+
+        //inserta valores
+        arbol.insertar(10);
+        arbol.insertar(20);
+        arbol.insertar(5);
+        arbol.insertar(4);  //provoca una rotacion
+        arbol.insertar(15); //provoca otra rotacion
+
+        //mostrar recorrido inorden
+        arbol.mostrarInorden();
+    }
 }
